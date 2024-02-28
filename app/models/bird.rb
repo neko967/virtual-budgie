7bd48d0caab1d7c16c2eeb3aad5f426c3f53c815
neko967 <113199838+neko967@users.noreply.github.com>
2024-02-28 11:23:50 +0900

    rubocop修正

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,4 +1,4 @@
 class Bird < ApplicationRecord
   enum bird_type: { ノーマル: 1, オパーリン: 2, レインボー: 3 }
-  has_many :user_having_birds
+  has_many :user_having_birds, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  has_many :user_having_birds
+
+  has_many :user_having_birds, dependent: :destroy
 end
