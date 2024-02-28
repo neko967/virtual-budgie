f5424fd901339152bfd271cb048f083f10cebd1f
neko967 <113199838+neko967@users.noreply.github.com>
2024-02-28 19:30:39 +0900

    インコ新規作成から、インコ詳細画面までの画面を作った

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :birds
-  resources :pets
+  resources :birds do
+    resources :pets, only: %i[create destroy]
+  end
+  resources :pets, only: %i[new index show edit]
 end
