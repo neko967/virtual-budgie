f298e07ea11659cf1f8eed1d8d43e8c9fd15f231
neko967 <113199838+neko967@users.noreply.github.com>
2024-02-28 14:28:30 +0900

    petsテーブルを作成

--- db/migrate/20240227134703_create_pets.rb
+++ db/migrate/20240227134703_create_pets.rb
@@ -1,6 +1,6 @@
-class CreateUserHavingBirds < ActiveRecord::Migration[7.1]
+class CreatePets < ActiveRecord::Migration[7.1]
   def change
-    create_table :user_having_birds do |t|
+    create_table :pets do |t|
       t.references :user, null: false, foreign_key: true
       t.references :bird, null: false, foreign_key: true
       t.string :name    , null: false
