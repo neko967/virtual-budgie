ae6facb5069930fc565b0f2afb5cef2f44e44642
neko967 <113199838+neko967@users.noreply.github.com>
2024-02-29 15:51:15 +0900

    REDNERデプロイ準備4

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,4 +6,3 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 # bundle exec rails db:migrate
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
