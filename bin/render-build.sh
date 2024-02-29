78010c082f53f7ed8ebcee584ea93ed7562b3164
neko967 <113199838+neko967@users.noreply.github.com>
2024-02-29 15:19:46 +0900

    データ投入済みのデプロイ準備修正２

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,4 +6,4 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
-bundle exec rails db:reset RAILS_ENV=production
+bundle exec rails db:reset
