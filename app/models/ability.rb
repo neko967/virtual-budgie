636efa35fffe50414084918441f3b2586dd0a005
neko967 <113199838+neko967@users.noreply.github.com>
2024-03-02 00:37:17 +0900

    cancancanの設定完了

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,10 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    if user.try(:admin?)
+      can :access, :rails_admin
+      can :manage, :all
+    end
     # Define abilities for the user here. For example:
     #
     #   return unless user.present?
