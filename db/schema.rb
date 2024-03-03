93639e0860bd3cfbc24cda948c46d130bf9bab9b
neko967 <113199838+neko967@users.noreply.github.com>
2024-03-04 00:43:30 +0900

    wordsテーブルを作った

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_01_144615) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_03_154020) do
   create_table "birds", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "bird_type", null: false
     t.text "description", null: false
@@ -43,6 +43,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_01_144615) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "words", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "word", null: false
+    t.bigint "pet_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pet_id"], name: "index_words_on_pet_id"
+  end
+
   add_foreign_key "pets", "birds"
   add_foreign_key "pets", "users"
+  add_foreign_key "words", "pets"
 end
