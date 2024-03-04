6406a72067a3ad16896cec02deee585281eeae8a
neko967 <113199838+neko967@users.noreply.github.com>
2024-03-04 21:08:09 +0900

    rails g controller rooms index show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'rooms/index'
+  get 'rooms/show'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: "staticpages#top"
