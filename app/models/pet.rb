b282c5dd5f29a44411a6ad1978bb24b1bfaa9ea2
neko967 <113199838+neko967@users.noreply.github.com>
2024-03-05 11:56:53 +0900

    Favoritesテーブルを作った

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,6 +2,7 @@ class Pet < ApplicationRecord
   belongs_to :user
   belongs_to :bird
   has_many :words, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   validates :name, length: { maximum: 255 }, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :pets, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   enum role: { general: 0, admin: 1 }
 
   def own?(object)
