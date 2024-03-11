b6081a284feff4c5c375d41da871a44350d0e6de
neko967 <113199838+neko967@users.noreply.github.com>
2024-03-11 23:34:51 +0900

    RSpecの初期設定を完了

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,16 @@ module AIbudgie
     config.load_defaults 7.1
     config.i18n.default_locale = :ja
 
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework :rspec,
+        fixtures: false, # テストDBにレコードを作るfixtureの作成をスキップ(FactoryBotを使用するため)
+        view_specs: false, # ビューファイル用のスペックを作成しない
+        helper_specs: false, # ヘルパーファイル用のスペックを作成しない
+        routing_specs: false # routes.rb用のスペックファイル作成しない
+    end
+    
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
