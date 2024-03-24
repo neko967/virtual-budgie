46092dab08ff8840ce560e7b9332b5e015e41dc1
neko967 <113199838+neko967@users.noreply.github.com>
2024-03-24 15:20:46 +0900

    インコに覚えさせる言葉を形態素分析するようにした

--- Gemfile
+++ Gemfile
@@ -73,6 +73,10 @@ gem 'aws-sdk-s3', require: false
 
 gem 'fog-aws'
 
+gem 'httparty' # APIリクエストを簡単に扱うためのgem
+
+gem 'dotenv-rails'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri windows]
---
+++ app/services/morphological_analysis_service.rb
@@ -0,0 +1,21 @@
+require 'httparty'
+
+class MorphologicalAnalysisService
+  include HTTParty
+  base_uri 'https://labs.goo.ne.jp/api/morph'
+
+  def self.analyze(text, app_id)
+    body = {
+      app_id: app_id,
+      sentence: text,
+      info_filter: 'form'
+    }.to_json
+
+    headers = {
+      'Content-Type' => 'application/json'
+    }
+
+    response = post('/', body: body, headers: headers)
+    response.parsed_response
+  end
+end
