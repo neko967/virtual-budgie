527fa692208813f344d6a5144a4e09f1f3eaefed
neko967 <113199838+neko967@users.noreply.github.com>
2024-04-02 23:28:11 +0900

    プロフィール画面を追加/プロフィールボタンとログアウトボタンをドロップダウンに入れた

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,9 @@
 class ProfilesController < ApplicationController
-  before_action :authenticate_user!, only: %i[edit update]
-  before_action :set_user, only: %i[edit update]
+  before_action :authenticate_user!, only: %i[show edit update]
+  before_action :set_user, only: %i[show edit update]
+
+  def show
+  end
 
   def edit
   end
